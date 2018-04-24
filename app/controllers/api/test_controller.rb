719d27971c1a17acabfe580ce3c97748242e61e3
Chris Nagy <christopher.nagy@metova.com>
2018-04-24 16:11:21 -0500

    test route

--- app/controllers/api/test_controller.rb
+++ app/controllers/api/test_controller.rb
@@ -1,5 +1,7 @@
-class TestController < ApplicationController
+class TestController < Api::BaseController
+  BOT_ID = '16d752b470ff5d9e8f1e2ffc49'
+  GROUP_ID = '40328258'
   def index
-
+    send_message(BOT_ID, 'Hello')
   end
 end
