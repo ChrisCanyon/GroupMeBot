7743ac92dad5221ed3bc082315cfe7972f0cf7d8
Chris Nagy <christopher.nagy@metova.com>
2018-04-24 16:14:25 -0500

    test send message

--- app/controllers/api/test_controller.rb
+++ app/controllers/api/test_controller.rb
@@ -1,7 +1,9 @@
-class TestController < Api::BaseController
-  BOT_ID = '16d752b470ff5d9e8f1e2ffc49'
-  GROUP_ID = '40328258'
-  def index
-    send_message(BOT_ID, 'Hello')
+module Api
+  class TestController < Api::BaseController
+    BOT_ID = '16d752b470ff5d9e8f1e2ffc49'
+    GROUP_ID = '40328258'
+    def index
+      send_message(BOT_ID, 'Hello')
+    end
   end
 end
