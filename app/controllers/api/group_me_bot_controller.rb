707fc4fa4e09b8dc3ec0ec4e567486be93271450
Chris Nagy <christopher.nagy@metova.com>
2018-04-24 16:36:48 -0500

    a

---
+++ app/controllers/api/group_me_bot_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  class GroupMeBotController < Api::BaseController
+    BOT_ID = '16d752b470ff5d9e8f1e2ffc49'
+    GROUP_ID = '40328258'
+    def index
+      process_message unless params['sender_type'] == 'bot' || (params['sender_type'] == 'system')
+    end
+
+    def process_message
+      send_message(BOT_ID, 'Hello')
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  root 'data_gatherer#index'
-  resources :data_gatherer
   namespace :api, defaults: { format: :json } do
-    post 'gnome_child', to: 'gnome_child#index'
-    post 'pax_bot', to: 'pax_bot#index'
-    post 'test', to: 'test#index'
+    post 'groupmebot/:bot_id', to: 'group_me_bot#index'
   end
 end
