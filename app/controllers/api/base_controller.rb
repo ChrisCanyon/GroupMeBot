2bab04a0abe5fda20cbf3598a77036afd48978e1
Chris Nagy <christopher.nagy@metova.com>
2018-04-25 17:40:28 -0500

    Generic function calling

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,6 +2,6 @@ module Api
   class BaseController < ::ApplicationController
     protect_from_forgery with: :null_session
     include GroupmeBotHelper
-    include CentralCommandCenterHelper
+    include CentralCommandCenter
   end
 end
