e640aae7cfcb84aae315b4d7181398af87280e66
Chris Nagy <christopher.nagy@metova.com>
2018-04-25 17:47:48 -0500

    Generic function calling

--- app/commands/central_command_center.rb
+++ app/commands/central_command_center.rb
@@ -7,10 +7,11 @@ module CentralCommandCenter
     @bot = bot
     @user = user
     @group_member = group_member
-
+    p 'here I am'
     send_message(@bot_id, "Permission Denied") && return if @group_member.access_level == "none"
     command = input[0]
     parameters = input[1..(input.count-1)] unless input.count < 2
     self.send(input[0], parameters)
+    p 'I done did it'
   end
 end
