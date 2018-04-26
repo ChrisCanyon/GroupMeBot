62be0354edeecb39136deb7fa79e0299179770cd
Chris Nagy <christopher.nagy@metova.com>
2018-04-26 17:54:04 -0500

    save

--- app/commands/central_command_center.rb
+++ app/commands/central_command_center.rb
@@ -31,9 +31,7 @@ module CentralCommandCenter
       next if @bot.active_libraries.include?(library)
       commands = LIBRARIES[library]
       unless commands.blank?
-        commands.each do |command|
-          @bot.active_commands << command
-        end
+        @bot.active_commands = @bot.active_commands + commands
         @bot.active_libraries << library
       end
     end
