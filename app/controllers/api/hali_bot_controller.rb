cab4f132ff07d4344aacc1af8fb28dcf4da342b7
Chris Nagy <chrisnagy10@yahoo.com>
2018-09-10 21:39:00 -0500

    made hali her own bot

--- app/controllers/api/hali_bot_controller.rb
+++ app/controllers/api/hali_bot_controller.rb
@@ -3,7 +3,7 @@ module Api
     require 'open-uri'
     require 'net/https'
 
-    BOT_ID = '6d9ee567d5dccdb1d416606199'
+    BOT_ID = 'b6514c53899b3fe8c4cc8452a8'
 
     def index
       process_message unless params['sender_type'] == 'bot' || (params['sender_type'] == 'system')
@@ -15,7 +15,7 @@ module Api
     end
 
     def check_command(escape_key)
-      send_message(BOT_ID, 'Meow') if params[:text].downcase[/(\bpax\b|\bpaxton\b)/]
+      send_message(BOT_ID, 'Purr') if params[:text].downcase[/(\bhali\b)/]
     end
   end
 end
