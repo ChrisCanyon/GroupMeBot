33b63338ea312253a1a8737501988208d913705e
Chris Nagy <chrisnagy10@yahoo.com>
2018-09-10 21:45:12 -0500

    oopsy poopsy

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     post 'groupmebot/:bot_id', to: 'group_me_bot#index'
     post 'paxbot', to: 'pax_bot#index'
-    post 'halibot', to: 'hali_bot#index'
   end
 end
