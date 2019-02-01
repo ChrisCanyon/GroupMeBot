50a76ed417acaa4623be8379961799e5f7e53322
Simon Winters <smwinters42@students.tntech.edu>
2019-02-01 12:48:18 -0600

    fixed new line

--- app/commands/runescape.rb
+++ app/commands/runescape.rb
@@ -7,7 +7,7 @@ module Runescape
     return send_message(@bot.bot_id, "Try '/runescape commands' for more options") unless parameters
     case parameters[0]
     when 'commands'
-      message = 'Commands: /' + RUNESCAPE_COMMANDS[1..(RUNESCAPE_COMMANDS.count-1)].join('\n/')
+      message = 'Commands: /' + RUNESCAPE_COMMANDS[1..(RUNESCAPE_COMMANDS.count-1)].join('\n')
       send_message(@bot.bot_id, message)
     end
   end
