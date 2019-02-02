2c2e83b4f88740f9418dccf4227346773f650abd
Chris Nagy <chrisnagy10@yahoo.com>
2019-02-01 20:50:55 -0600

    debug

--- app/commands/admin_commands.rb
+++ app/commands/admin_commands.rb
@@ -10,27 +10,21 @@ module AdminCommands
     send_message(@bot_id, "Permission Denied") && return unless @user.access_level == 'admin'
     return send_message(@bot_id, "Try '!commands' for more options") unless command
 
-    case command[0]
-    when ADMIN_COMMANDS[0]
-      test_function()
-    when ADMIN_COMMANDS[1]
-      revoke()
-    when ADMIN_COMMANDS[2]
-      grant()
-    when ADMIN_COMMANDS[3]
-      commands()
+    if ADMIN_COMMANDS.include?(command[0])
+      parameters = command[1..(command.count-1)] unless command.count < 2
+      self.send(input[0], parameters)
     else
       send_message(@bot_id, "Invalid Command")
     end
   end
 
   private
-  def commands
+  def commands(parameters = nil)
     message = "Commands: \n/" +  ADMIN_COMMANDS[1..(ADMIN_COMMANDS.count-1)].join("\n/")
     send_message(@bot_id, message)
   end
 
-  def grant
+  def grant(parameters = nil)
     send_message(@bot_id, "Usage: Tag someone to grant") && return unless valid_permission_change_params?
     groupme_ids = params[:attachments][0][:user_ids]
     groupme_ids.each do |id|
@@ -40,7 +34,7 @@ module AdminCommands
     send_message(@bot_id, "Permission(s) granted")
   end
 
-  def revoke
+  def revoke(parameters = nil)
     send_message(@bot_id, "Usage: Tag someone to revoke") && return unless valid_permission_change_params?
     groupme_ids = params[:attachments][0][:user_ids]
     p "\n\n\n" + groupme_ids + "\n\n\n"
@@ -55,7 +49,7 @@ module AdminCommands
     params[:attachments].present? && params[:attachments][0][:type] == "mentions"
   end
 
-  def test_function
+  def test_function(parameters = nil)
     sample = User.all.sample
     attachments = [{ "loci": [[0, sample.name.length + 1]], "type": "mentions", "user_ids": ["#{sample.groupme_id}"] }]
     send_message(@bot_id, "@#{sample.name}", attachments)
