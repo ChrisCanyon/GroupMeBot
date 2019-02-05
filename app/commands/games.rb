ac1aaca136b3740ff16e041005bec7a1f2bb646d
Chris Nagy <chrisnagy10@yahoo.com>
2019-02-05 10:42:17 -0600

    Update games.rb

--- app/commands/games.rb
+++ app/commands/games.rb
@@ -62,7 +62,7 @@ module Games
     if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '*')
       return true
     end
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][0] != '*')
+    if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '*')
       return true
     end
     return false
