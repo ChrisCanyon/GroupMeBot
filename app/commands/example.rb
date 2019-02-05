139d3a7a241670f7f6b7abdf0941f06b2f2bcaf3
Chris Nagy <chrisnagy10@yahoo.com>
2019-02-05 09:52:38 -0600

    tictac

--- app/commands/example.rb
+++ app/commands/example.rb
@@ -1,25 +1,45 @@
 module LibraryName
   include GroupmeBotHelper
 
-  LIBRARYNAME_COMMANDS = [:library_name, :command1, :command2, :commandX]
+  LIBRARYNAME_COMMANDS = [:library_name, :start_tic_tac_toe]
 
   def library_name(parameters = nil)
     case parameters[0]
     when 'commands'
-      message = 'Commands: /' + LIBRARYNAME_COMMANDS[1..(LIBRARYNAME_COMMANDS.count-1)].join('\n/')
+      message = "Commands: \n/" + LIBRARYNAME_COMMANDS[1..(LIBRARYNAME_COMMANDS.count-1)].join("\n/")
       send_message(@bot.bot_id, message)
     end
   end
 
-  def command1(parameters = nil)
+  def start_tic_tac_toe(parameters = nil)
+    board = "*|*|*\n*|*|*\n*|*|*\n"
+    message = "+play_tic_tac_toe X\n" + board
+    send_message(@bot.bot_id, message)
+  end
+
+  def play_tic_tac_toe(parameters = nil)
+    if (parameters.count == 0 || ['X', 'O'].include?(parameters[0]))
+      return send_message(@bot.bot_id, "Invalid request. Required 'X' or 'O' not found")
+    end
+    message = "+play_tic_tac_toe "
+    if (parameters[0] == 'X')
+      message += "O\n"
+    else
+      message += "X\n"
+    end
+
+    board = parse_tic_tac_toe_board(parameters[1..(parameters.count-1)])
 
   end
 
-  def command2(parameters = nil)
+  private
+
+  def parse_tic_tac_toe_board(board)
+      p board
+  end
+
+  def build_board_from_array(array)
 
   end
 
-  def commandX(parameters = nil)
-
-  end
 end
---
+++ app/commands/games.rb
@@ -0,0 +1,25 @@
+module Games
+  include GroupmeBotHelper
+
+  GAMES_COMMANDS = [:library_name, :command1, :command2, :commandX]
+
+  def Games(parameters = nil)
+    case parameters[0]
+    when 'commands'
+      message = "Commands: \n/" + GAMES_COMMANDS[1..(GAMES_COMMANDS.count-1)].join("\n/")
+      send_message(@bot.bot_id, message)
+    end
+  end
+
+  def command1(parameters = nil)
+
+  end
+
+  def command2(parameters = nil)
+
+  end
+
+  def commandX(parameters = nil)
+
+  end
+end
